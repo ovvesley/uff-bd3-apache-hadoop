d77709d2d96c2395c5084dee8f2c9b7336928de8
Wesley <wesley.ferreira@businessmonster.com.br>
2023-12-09 14:20:26 -0300

    use example haddopp site

--- src/main/java/br/com/ggvd/contabigrama/ContaBigrama.java
+++ src/main/java/br/com/ggvd/contabigrama/ContaBigrama.java
@@ -1,66 +1,61 @@
 package br.com.ggvd.contabigrama;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class ContaBigrama {
 
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+    public static void main(String[] args) throws IOException,
+            InterruptedException, ClassNotFoundException {
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+        System.out.println("---- STARTER CONTA BIGRAMA ----");
 
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
-        }
-    }
+        // Captura o parâmetros passados após o nome da Classe driver.
+        Path inputPath = new Path(args[0]);
+        Path outputDir = new Path(args[1]);
 
-    public static class IntSumReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private IntWritable result = new IntWritable();
+        // Criar uma configuração
+        Configuration conf = new Configuration(true);
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            result.set(sum);
-            context.write(key, result);
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("--------------------STARTER CONTA BIGRAMA1---------------------");
-
-        Configuration conf = new Configuration();
-        conf.addResource(new Path("/usr/local/hadoop-3.3.5/etc/hadoop/core-site.xml"));
-        Job job = Job.getInstance(conf, "word count");
+        // Criar o job
+        Job job = new Job(conf, "ContaPalavras");
         job.setJarByClass(ContaBigrama.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+
+        // Definir classes para Map e Reduce
+        job.setMapperClass(ContaBigramaMapper.class);
+        job.setReducerClass(ContaBigramaReducer.class);
+        job.setNumReduceTasks(1);
+
+        // Definir as chaves e valor
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        // Entradas
+        FileInputFormat.addInputPath(job, inputPath);
+        job.setInputFormatClass(TextInputFormat.class);
+
+        // Saidas
+        FileOutputFormat.setOutputPath(job, outputDir);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        // EXcluir saida se existir
+        FileSystem hdfs = FileSystem.get(conf);
+        if (hdfs.exists(outputDir))
+            hdfs.delete(outputDir, true);
+
+        // Executa job
+        int code = job.waitForCompletion(true) ? 0 : 1;
+        System.exit(code);
+
     }
 }
